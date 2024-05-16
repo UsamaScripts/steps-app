edb9eef3d9b70b737da0e1c5de566d581ac72cb1
Muhammad Usama <usamaj2106@gmail.com>
2024-05-16 13:10:47 +0500

    created dynamicly change step with using useState management

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function App() {
   const messages = [
     "Learn React ⚛️",
@@ -5,14 +7,20 @@ function App() {
     "Invest your new income 🤑",
   ];
 
-  const step = 3;
+  const [step, setStep] = useState(1);
+  function HandleNext() {
+    if (step < 3) setStep(step + 1);
+  }
+  function HandlePrevous() {
+    if (step > 1) setStep(step - 1);
+  }
 
   return (
     <div className="steps">
       <div className={"numbers"}>
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
       <p className="message">
         Step {step}:{messages[step - 1]}
@@ -20,13 +28,13 @@ function App() {
 
       <div className="buttons">
         <button
-          className=""
+          onClick={HandlePrevous}
           style={{ backgroundColor: "#7950f2", color: "#fff" }}
         >
           Prevoius
         </button>
         <button
-          className=""
+          onClick={HandleNext}
           style={{ backgroundColor: "#7950f2", color: "#fff" }}
         >
           Next
