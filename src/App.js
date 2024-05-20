efddf8a410b00daa4d69f085efc312d035606add
Muhammad Usama <usamaj2106@gmail.com>
2024-05-20 14:40:24 +0500

    created reuseable massge commponenet with childprops

--- src/App.js
+++ src/App.js
@@ -37,9 +37,7 @@ function App() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}:{messages[step - 1]}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
 
           <div className="buttons">
             {/* <button
@@ -65,6 +63,15 @@ function App() {
         </div>
       )}
     </>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3> Step {step} </h3>
+      {children}
+    </div>
   );
 }
 function Button({ onClick, bg, textColor, children }) {
